e85dc9a2e4f9dbb7633d3bcaef6cb69a682d9daa
ERoyson <94379543+ERoyson@users.noreply.github.com>
2022-02-21 15:00:35 +0100

    Update prague_parking2.sql

--- prague_parking2.sql
+++ prague_parking2.sql
@@ -17,6 +17,14 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+ 
+
+DROP DATABASE IF EXISTS `prague_parking2`;
+
+Create Database `prague_parking2`;
+
+USE `prague_parking2`;
+
 --
 -- Database: `prague_parking2`
 --
