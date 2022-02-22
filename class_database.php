005cafa1a2e4d4f12ff2a7cbaeceb4e3a56dd567
ERoyson <94379543+ERoyson@users.noreply.github.com>
2022-02-22 19:57:01 +0100

    Fräääsch

--- class_database.php
+++ class_database.php
@@ -7,7 +7,7 @@ class Database
     const PASSWORD = "";
     const DBNAME = "prague_parking2";
 
-    public static function open() // Open connection to DB-server.
+       public static function open() // Open connection to DB-server.
     {
         $connection = new mysqli(self::SERVERNAME, self::USERNAME,self::PASSWORD,self::DBNAME);
 
@@ -18,7 +18,8 @@ class Database
         return $connection;
     }
 
-    // INSERT ParkingSpot  - Eric
+
+    // Insert ParkingSpots - Eric
     public static function Insert_ParkingSpots($size)
     {
         $conn = self::open();
@@ -43,7 +44,8 @@ class Database
         $conn->close();
         return "Inserted successfully";  // return $_SESSION['message'] ? to display message freely
     }
-    
+
+        
     //insert into Vehicle - Olle
     public static function Insert_Vehicle($vehicletypeid, $regnr)
     {
@@ -53,7 +55,9 @@ class Database
         $stmt->bind_param("is", $vehicletypeid, $regnr);
 
         if ($stmt->execute()) {
+          $last_id = $conn->insert_id;
           echo "Insert was successfull!";
+          return $last_id;
         } 
         else {
           echo "Error: " . $stmt . "<br>" . $conn->error;
@@ -62,7 +66,9 @@ class Database
         $conn->close();
     }
     
-      public static function insert_vehicleType()
+
+    // Insert VehicleTypes - Andreas
+      public static function Insert_vehicleType()
     {
         $con = self::open();
 
@@ -85,26 +91,27 @@ class Database
         $con->close();
 
     }
-    
-    
-      public static function Insert_Log($parkingmomentid, $cost, $timeexit)
-      {
-          $conn = self::open();
-  
-          $stmt = $conn->prepare("INSERT INTO `Log`(ParkingmomentID, Cost, TimeExit) VALUES(?,?,?);");
-          $stmt->bind_param("iis", $parkingmomentid, $cost, $timeexit);
-  
-          if ($stmt->execute()) {
-            echo "Insert was successfull!";
-          } 
-          else {
-            echo "Error: " . $stmt . "<br>" . $conn->error;
-          }
-  
-          $conn->close();
-      } 
-            //insert into vehicle Moments
-    public static function insert_vehicleMoments($parkingSpot, $VehicleID, $TimeArrival)
+
+    // INSERT INTO LOG - Olle
+    public static function Insert_Log($parkingmomentid, $cost, $timeexit)
+    {
+        $conn = self::open();
+
+        $stmt = $conn->prepare("INSERT INTO `Log`(ParkingmomentID, Cost, TimeExit) VALUES(?,?,?);");
+        $stmt->bind_param("iii", $parkingmomentid, $cost, $timeexit);
+
+        if ($stmt->execute()) {
+          echo "Insert was successfull!";
+        } 
+        else {
+          echo "Error: " . $stmt . "<br>" . $conn->error;
+        }
+
+        $conn->close();
+    } 
+
+    //insert into vehicle Moments
+    public static function insert_ParkingMoments($parkingSpot, $VehicleID, $TimeArrival)
     {
         $conn = self::open();
   
@@ -123,12 +130,12 @@ class Database
     }
     
           //visa parkerade fordon
-          public function ShowParkedVehicles()
+    public static function ShowParkedVehicles()
       {
         $conn = self::open();
 
         $sql = <<<'SQL'
-        SELECT v.RegNr, vt.Type, pm.TimeArrival
+        SELECT pm.ParkingSpotID, v.RegNr, vt.Type, pm.TimeArrival
         FROM parkingmoments AS pm
         INNER JOIN vehicle AS v ON pm.VehicleID = v.VehicleID
         INNER JOIN vehicletype AS vt ON v.VehicleTypeID = vt.VehicleType;
@@ -140,75 +147,17 @@ class Database
         if($result->num_rows > 0)
         {
             while($row = $result->fetch_assoc()) {
-                echo $row['RegNr'] . " | " . $row['Type'] . " | " . $row['TimeArrival'] . "<br>";
+                echo $row['ParkingSpotID'] . " | " . $row['RegNr'] . " | " . $row['Type'] . " | " . $row['TimeArrival'] . "<br>";
               }
         }
         else{
             echo "No parked vehicles found!";
         }
-              $conn->close();
         
       }
     
-    
-    //----------------FEL FEL FEL FEL FEL------------------------------------------DENNA METODEN OCH NÄSTA :(
-    public function Get_parkingMomentID($regNr)
-    {
-      $conn = self::open();
-      $spotID = self::dbSearchRegNr($regNr);
-      if($spotID == -1){
-        $conn->close();
-        throw new mysqli_sql_exception("fordonet har inte parkerats");
-      }
-
-      $sql = <<<'SQL'
-      SELECT pm.parkingmomentsID
-      FROM vehicle AS v
-      INNER JOIN parkingMoments AS pm ON v.VehicleID = pm.VehicleID
-      INNER JOIN ParkingSpot as ps ON pm.ParkingSpotID = ps.ParkingSpotID
-      WHERE  regNr =   
-      SQL;
-
-      $sql.= '"'.$regNr.'"'.";";
-      $result = mysqli_query($conn, $sql);
-
-      if ($result->num_rows > 0) 
-          {
-            while($row = $result->fetch_assoc()) 
-            {
-
-              $momentID = $row["parkingmomentsID"];
-              return $momentID;
-            }
-          } 
-          else
-          {
-            throw new mysqli_sql_exception("0 rows returned");
-          }
-      
-      $conn->close();
-      //"spot" och "vehicle" ska skickas vidare
-    }
-
-
-    public function DeleteParkedVehicle($regNr)
-    {
-      $momentID = self::Get_parkingMomentID($regNr);
-      $conn = self::open();
-
-      $stmt = $conn->prepare("INSERT TO Log WHERE parkingmomentsID = ?");
-      $stmt->bind_param("i", $momentID);
-
-      $stmt2 = $conn->prepare("DELETE FROM ParkingMoments WHERE parkingmomentsID = ?");
-      $stmt2->bind_param("i", $momentID);
-  
-      $conn->close();
-
-    }
-//----------------FEL FEL FEL FEL FEL------------------- DOM TVÅ ÅVANSTÅENDE METODERNA SUUUUUGER
-    
     //search for regnr db
-     public function dbSearchRegNr($regnr)
+     public static function dbSearchRegNr($regnr)
       {
           $regnr = strtoupper($regnr);
           $conn = self::open();
@@ -237,9 +186,10 @@ class Database
       }
     
        //find empty spot
-       public function findemptyspot($vehicle)
+       public static function findemptyspot($vehicle)
       {
         $conn = self::open();
+
         $sql = <<<'SQL'
             SELECT ps.ParkingSpotID, ps.SpotSize
             FROM parkingspot AS ps
@@ -261,8 +211,8 @@ class Database
         $conn->close();
         return -1;   
     }
-    
-     public static function Get_SpotSize($spot)
+
+    public static function Get_SpotSize($spot)
     {
         $conn = self::open();
 
@@ -348,10 +298,181 @@ class Database
             throw new mysqli_sql_exception("Error creating table: " . $conn->error);
         }
      
+    } 
+
+    public static function Park_vehicle_DB($vehicle)
+    {
+        $typeID = 0;
+        $toa = date('Y-m-d H:i:s');
+        switch($vehicle->get_vehicleType())
+        {
+            case 'Car':
+                {
+                    $typeID = 1;
+                    break;
+                }
+            case 'MC':
+                {
+                    $typeID = 2;
+                    break;
+                }
+            case 'Bike':
+                {
+                    $typeID = 3;
+                    break;
+                }
+        }
+
+        $check = self::dbSearchRegNr($vehicle->get_regnr());
+
+        if($check === -1) // OM FORDONET INTE HAR PARKERAT
+        {
+            // Leta ledig plats
+            $emptySpot = self::findemptyspot($vehicle);
+            if($emptySpot === -1)
+            {
+                throw new mysqli_sql_exception("Det finns ingen plats för fordonet");
+            }
+            $vehicleID = self::Insert_Vehicle($typeID,$vehicle->get_regnr());
+            self::insert_ParkingMoments($emptySpot,$vehicleID,$toa);
+            self::Update_SpotSize_Sub($emptySpot,$vehicle);
+
+            return "Vehicle has parked";
+        }
+        else
+        {
+            throw new mysqli_sql_exception("Fordonet har redan parkerat här!");
+        }
     }
-    
-    
-    // FUNGERAR NU!
+
+    // ANDREAS
+    public static function Get_parkingMomentID($regNr)
+    {
+      $conn = self::open();
+      $spotID = self::dbSearchRegNr($regNr);
+      if($spotID == -1)
+      {
+        $conn->close();
+        throw new mysqli_sql_exception("fordonet har inte parkerats");
+      }
+
+      $sql = <<<'SQL'
+      SELECT pm.parkingmomentsID
+      FROM vehicle AS v
+      INNER JOIN parkingMoments AS pm ON v.VehicleID = pm.VehicleID
+      INNER JOIN ParkingSpot as ps ON pm.ParkingSpotID = ps.ParkingSpotID
+      WHERE  regNr =   
+      SQL;
+
+      $sql.= '"'.$regNr.'"'.";";
+      $result = mysqli_query($conn, $sql);
+
+      if ($result->num_rows > 0) 
+          {
+            while($row = $result->fetch_assoc()) 
+            {
+
+              $momentID = $row["parkingmomentsID"];
+              return $momentID;
+            }
+          } 
+          else
+          {
+            throw new mysqli_sql_exception("0 rows returned");
+          }
+      
+      $conn->close();
+      //"spot" och "vehicle" ska skickas vidare
+    }
+
+    public static function Get_TotalCost($regNr, $timeexit)
+    {
+        $conn = self::open();
+
+        $sql = "SELECT pm.timearrival, v.vehicletypeID FROM parkingmoments as pm
+        INNER JOIN vehicle as v ON pm.vehicleID = v.vehicleID
+        WHERE v.regNr = '$regNr';";
+
+        $result = $conn->query($sql);
+        if ($result->num_rows > 0) 
+        {
+            while($row = $result->fetch_array())
+            {
+                $value[] = $row[0]; //sparar tidigare plats
+                $value[] = (int)$row[1];
+            }
+        }
+        else
+        {
+            $conn->close();
+            throw new mysqli_sql_exception("0 rows returned");
+        }
+        
+        $conn->close();
+
+        $cost = 0;
+        switch($value[1])
+        {
+            case 1:
+                {
+                    $cost = 40;
+                    break;
+                }
+            case 2:
+                {
+                    $cost = 20;
+                    break;
+                }
+            case 3:
+                {
+                    $cost = 5;
+                    break;
+                }
+            default:
+                $cost=100;
+        }
+
+        $date1 = $value[0];
+        $date2 = $timeexit;
+
+        $timestamp1 = strtotime($date1);
+        $timestamp2 = strtotime($date2);
+        $hour = abs($timestamp2 - $timestamp1)/(60*60);
+        $hour = ceil($hour);
+
+        $price = $cost * $hour;
+        return $price;
+    }
+    // ANDREAS
+    public static function DeleteParkedVehicle($regNr)
+    {
+      $momentID = self::Get_parkingMomentID($regNr);
+      $timeexit = date('Y-m-d H:i:s');
+      $cost = self::Get_TotalCost($regNr,$timeexit);
+
+      $conn = self::open();
+
+      $stmt = $conn->prepare("INSERT INTO Log (parkingmomentID, cost, TimeExit) VALUES (?,?,?)");
+      $stmt->bind_param("iis", $momentID, $cost, $timeexit);
+
+      $stmt2 = $conn->prepare("DELETE FROM ParkingMoments WHERE parkingmomentsID = ?");
+      $stmt2->bind_param("i", $momentID);
+  
+      if($stmt->execute())
+      {
+          echo "YAY!";
+      }
+      if($stmt2->execute())
+      {
+          echo "JIPPIE!";
+      }
+
+      $conn->close();
+
+    }
+
+
+    // OLLE
     public static function moveVehicle($vehicle, $ParkingSpotID)
     {
         $conn = self::open();
@@ -361,7 +482,7 @@ class Database
         $sqlfindlastspot =
         "SELECT ParkingSpotID
         FROM parkingmoments 
-        WHERE parkingmomentsID = '$momentID';";
+        WHERE parkingmomentsID = $momentID;";
 
         $sqlupdate = 
         "UPDATE parkingmoments AS pm
@@ -376,15 +497,11 @@ class Database
         if($spaceleft >= 0)
         {
             $result = $conn->query($sqlfindlastspot);
-
             if ($result->num_rows > 0) 
             {
-                while($row = $result->fetch_assoc()) 
-                {
-                    $lastspot = $row["ParkingSpotID"]; //sparar tidigare plats
-                } 
+                $row = $result->fetch_assoc();
+                $lastspot = $row["ParkingSpotID"]; //sparar tidigare plats
             }
-            
             self::Update_SpotSize_Add($lastspot, $vehicle); //uppdatera tidigare plats
             self::Update_SpotSize_Sub($ParkingSpotID, $vehicle); //ändra storlek på ny plats
             
@@ -398,8 +515,6 @@ class Database
         $conn->close();
           return "No space left on parkingspot: " . $ParkingSpotID;
         }
-        
     }
-    
 }
 ?>
